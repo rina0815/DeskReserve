596cb9c7d507c12d5fa26f0d53cdb7103e85ac3b
rina0815 <iwtrn.0815@gmail.com>
2022-04-13 20:40:19 +0900

    vuetifyの導入

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,12 @@ require('./bootstrap');
 import Vue from 'vue';
 import router from './router';
 import App from './App.vue';
+import vuetify from './vuetify';
 
 new Vue({
 el: '#app',
 	router,
+	vuetify,
 	components: { App },
 	template: '<App />'
 })
